ed68b15ad175aaa513a5e97094f370608eeeee2c
teodor <teodorzacke@gmail.com>
2019-05-22 17:28:35 +0200

    yeehaw sänkte gränsen

--- testingdata.py
+++ testingdata.py
@@ -8,13 +8,13 @@ Created on Wed May 22 15:52:45 2019
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("/home/teodor/Documents/birds/testfile.csv", delim_whitespace=True, names=("Date", "Time", "Data"))
+data = pd.read_csv("/home/teodor/Documents/birds/testfile.txt", delim_whitespace=True, names=("Date", "Time", "Data"))
 
 dota = pd.DataFrame(columns=["Time", "Data"])
 dota.Time = data.Time
 print(data)
 for i in range(len(data.Data)-1):
-    if data.Data[i+1] - data.Data[i] < 0:
+    if data.Data[i+1] - data.Data[i] < 0 or data.Data[i+1] - data.Data[i] >= 20:
         dota.Data.loc[i+1] = 0
     elif data.Data[i+1] - data.Data[i] >= 4:
         dota.Data.loc[i+1] = 4
